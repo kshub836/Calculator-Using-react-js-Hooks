ef7e825d520146a2555a2079e5562b6cb1ce2f3d
Shubham kumar <71190308+kshub836@users.noreply.github.com>
2022-12-09 11:38:54 +0530

    calculator using reactjs

---
+++ src/App.js
@@ -0,0 +1,140 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [result, setResult] = useState("");
+
+  const clickHandler = (event) => {
+    setResult(result.concat(event.target.value));
+  };
+  const clearDisplay = () => {
+    setResult("");
+  };
+  const calculate = () => {
+    setResult(eval(result).toString());
+  };
+
+  const backspace = () => {
+    setResult(result.slice(0,result.length - 1));
+  };
+
+  return (
+    <div className="container">
+      
+    <div className="calci">
+      <input type="text" placeholder="0" id="shubh" value={result} />
+      <input type="button" value="AC" className="AC" onClick={clearDisplay} />
+      <input type="button" 
+      value="C"
+      className="Button2" 
+      onClick={backspace}
+      id="backspace"
+       />
+       <input
+        type="button"
+        value="%"
+        className="Button2"
+        onClick={clickHandler}
+      />     
+      <input
+        type="button"
+        value="/"
+        className="Button2"
+        onClick={clickHandler}
+      />      
+      <input
+        type="button"
+        value="9"
+        className="button"
+        onClick={clickHandler}
+      />
+      <input
+        type="button"
+        value="8"
+        className="button"
+        onClick={clickHandler}
+      />
+      <input
+        type="button"
+        value="7"
+        className="button"
+        onClick={clickHandler}
+      />
+      <input
+        type="button"
+        value="*"
+        className="Button2"
+        onClick={clickHandler}
+      />
+      <input
+        type="button"
+        value="6"
+        className="button"
+        onClick={clickHandler}
+      />
+      <input
+        type="button"
+        value="5"
+        className="button"
+        onClick={clickHandler}
+      />
+      <input
+        type="button"
+        value="4"
+        className="button"
+        onClick={clickHandler}
+      /><input
+      type="button"
+      value="+"
+      className="Button2"
+      onClick={clickHandler}
+    />
+       
+      <input
+        type="button"
+        value="3"
+        className="button"
+        onClick={clickHandler}
+      />
+      <input
+        type="button"
+        value="2"
+        className="button"
+        onClick={clickHandler}
+      />
+      <input
+        type="button"
+        value="1"
+        className="button"
+        onClick={clickHandler}
+      />
+      <input
+        type="button"
+        value="-"
+        className="Button2"
+        onClick={clickHandler}
+      />
+      <input
+        type="button"
+        value="0"
+        className="button"
+        onClick={clickHandler}
+      />
+      <input
+        type="button"
+        value="."
+        className="Button2"
+        onClick={clickHandler}
+      />
+      <input
+        type="button"
+        value="="
+        className="button button1"
+        onClick={calculate}
+      />
+    </div>
+    </div>
+  );
+};
+
+export default App;
